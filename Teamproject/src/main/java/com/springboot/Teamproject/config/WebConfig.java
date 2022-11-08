87737b68b2ed2756263c369c97b4ca55c2fca26f
wwt5744 <ghfty44@naver.com>
2022-11-08 17:34:59 +0900

    이미지 즉시 출력 기능 추가

--- Teamproject/src/main/java/com/springboot/Teamproject/config/WebConfig.java
+++ Teamproject/src/main/java/com/springboot/Teamproject/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/img/**")
+        registry.addResourceHandler("/**")
                 .addResourceLocations("file:src/main/resources/static/");
     }
 }
